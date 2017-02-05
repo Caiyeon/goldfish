a9a03e370e7afcc84efc1bf74084fb16a99e0c17
Tony Cai <081099@gmail.com>
2017-02-05 14:57:23 -0800

    updated footer and package info

--- frontend/client/components/layout/FooterBar.vue
+++ frontend/client/components/layout/FooterBar.vue
@@ -8,14 +8,15 @@
               <i class="fa fa-github"></i>
             </span>
           </a>
-          <a href="https://twitter.com/_fundon">
+          <a>
             <span class="icon">
               <i class="fa fa-twitter"></i>
             </span>
           </a>
         </p>
-        <p><span class="icon"><i class="fa fa-code"></i></span> with <span class="icon"><i class="fa fa-heart"></i></span> by <a href="https://github.com/fundon">fundon</a>.</p>
+        <p><span class="icon"><i class="fa fa-code"></i></span> with <span class="icon"><i class="fa fa-heart"></i></span> by <a href="https://github.com/caiyeon">caiyeon</a>.</p>
         <p>Code licensed under <a :href="'https://github.com/' + repository + '/blob/master/LICENSE'">{{ license }}</a>.</p>
+        <p>Frontend boilerplate made by <a href="https://github.com/fundon">fundon</a>.</p>
       </div>
     </div>
   </footer>
@@ -36,11 +37,11 @@ export default {
 
 .footer {
   margin-left: 180px;
-  
+
   @include mobile() {
     margin-left: 0;
   }
-  
+
   .social a {
     border-bottom: none !important;
   }
