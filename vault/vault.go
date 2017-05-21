b16d9271859282bfa598132f6d11e39dbbd4d69f
Caiyeon <081099@gmail.com>
2017-05-21 12:15:13 -0700

    Allow config_path override when server launches in dev mode

--- vault/vault.go
+++ vault/vault.go
@@ -36,12 +36,15 @@ func init() {
 	flag.StringVar(&vaultAddress, "vault_addr", "http://127.0.0.1:8200", "Vault address")
 	flag.StringVar(&wrappingToken, "vault_token", "", "The approle secret_id (must be in the form of a wrapping token)")
 	flag.StringVar(&rolePath, "approle_path", "auth/approle/login", "The approle mount's login path")
-	flag.StringVar(&configPath, "config_path", "secret/goldfish", "A generic backend endpoint to store run-time settings")
+	flag.StringVar(&configPath, "config_path", "", "A generic backend endpoint to store run-time settings. E.g. 'secret/goldfish'")
 	flag.StringVar(&roleID, "role_id", "goldfish", "The approle role_id")
 
 	flag.Parse()
 	if vaultAddress == "" || wrappingToken == "" {
-		panic("Invalid cmd args. See --help for details")
+		panic("Vault address and wrapped accessor missing. See --help for details")
+	}
+	if configPath == "" && !DevMode {
+		panic("config_path must be set unless dev mode is enabled")
 	}
 
 	// setup server's vault client, used for login transit encryption/decryption
@@ -57,7 +60,7 @@ func init() {
 	vaultClient.SetAddress(vaultAddress)
 	vaultClient.SetToken(vaultToken)
 
-	if DevMode {
+	if DevMode && configPath == "" {
 		loadDevModeConfig()
 	} else {
 		// load config once to ensure validity
