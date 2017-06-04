c735af1cc00971b6ccbf8e9a5886428065431b79
Yonni Luu <yonni.luu@gmail.com>
2017-06-03 19:55:55 -0700

    ompleted general layout of page, unwrap button is only enabled when there is an  token input

--- frontend/client/views/tools/Wrapper.vue
+++ frontend/client/views/tools/Wrapper.vue
@@ -3,6 +3,108 @@
     <div class="tile is-ancestor">
       <div class="tile is-parent">
         <article class="tile is-child box">
+          <label class="label">Token</label>
+          <div class="field has-addons">
+            <p class="control is-expanded">
+              <input class="input" type="text"
+                     placeholder="Paste token here to unwrap"
+                     v-model=currToken
+              >
+            </p>
+            <p class="control is-pulled-right">
+            <a class="button is-primary"
+            @click="unWrapToken()"
+            :disabled="currToken === ''">
+            <span>Unwrap</span>
+            </a>
+            </p>
+          </div>
+          <label class="label"> Data </label>
+          <article class="tile is-child box">
+
+            <!-- data table -->
+
+            <div class="table-responsive">
+              <table class="table is-striped is-narrow">
+
+                <!-- header -->
+
+                <thead>
+                  <tr is-expandedß>
+                    <th>Key</th>
+                    <th>Value</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <!-- body -->
+                <tbody>
+                  <tr>
+                    <!-- Editable key field -->
+                    <td>
+                      <p class="control">
+                        <input class="input is-small" type="text" placeholder="">
+                      </p>
+                    </td>
+
+                    <!-- Editable value field -->
+                    <td>
+                      <p class="control">
+                        <input class="input is-small" type="text" placeholder="">
+                      </p>
+                    </td>
+
+                    <td width="30">
+                      <a @click="deleteItem(index)">
+                        <span class="icon">
+                          <i class="fa fa-times-circle"></i>
+                        </span>
+                      </a>
+                    </td>
+
+                  </tr>
+
+                  <!-- new key value pair insertion row -->
+                  <tr>
+                    <td>
+                      <p class="control">
+                        <input
+                        class="input is-small"
+                        type="text"
+                        placeholder="Add a key to wrap"
+                        v-model="newKey"
+                        v-bind:class="[
+                        newKey === '' ? '' : 'is-success',
+                        newKeyExists ? 'is-danger' : '']"
+                        >
+                      </p>
+                    </td>
+                    <td>
+
+                      <p class="control">
+                        <input
+                        class="input is-small"
+                        type="text"
+                        placeholder="Add a value to wrap"
+                        v-model="newValue"
+                        v-bind:class="[newValue === '' ? '' : 'is-success']"
+                        >
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot>
+
+                </tfoot>
+              </table>
+            </div>
+            <p class="control">
+              <a class="button is-primary"
+              @click="wrapData()"
+              :disabled="tableData === ''">
+              <span>Wrap</span>
+            </a>
+          </p>
+          </article>
         </article>
       </div>
     </div>
@@ -13,7 +115,9 @@
 export default {
   data () {
     return {
-      csrf: ''
+      csrf: '',
+      tableData: [],
+      currToken: ''
     }
   },
 
@@ -26,6 +130,17 @@ export default {
   },
 
   methods: {
+    wrapData: function (argument) {
+      // body...
+    },
+
+    unWrapToken: function (argument) {
+      // body...
+    },
+
+    deleteItem: function (index) {
+      this.tableData.splice(index, 1)
+    }
   }
 }
 </script>
