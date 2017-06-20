ec4b6160081150321d76a6f2c4481fe0abc40173
Tony Cai <081099@gmail.com>
2017-06-19 17:48:20 -0700

    Added #56

--- frontend/client/views/tools/CreateToken.vue
+++ frontend/client/views/tools/CreateToken.vue
@@ -265,7 +265,7 @@
         <!-- Right column -->
         <div class="column is-6">
 
-          <!-- If user does not have capability to list roles -->
+          <!-- Warnings -->
           <div v-if="availableRoles === null" class="field">
             <article class="message is-warning">
               <div class="message-body">
@@ -273,12 +273,18 @@
               </div>
             </article>
           </div>
-
-          <!-- Role warning -->
           <div v-if="selectedRole" class="field">
             <article class="message is-warning">
               <div class="message-body">
                 <strong>Warning: Some options may be overridden by role details</strong>
+              </div>
+            </article>
+          </div>
+          <div class="field"
+          v-if="this.max_ttl != '' && (stringToSeconds(this.ttl) > stringToSeconds(this.max_ttl))">
+            <article class="message is-warning">
+              <div class="message-body">
+                <strong>Warning: ttl is longer than max_ttl</strong>
               </div>
             </article>
           </div>
