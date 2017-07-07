abf8fab0434924992b91be3f8988b2708431f126
Caiyeon <081099@gmail.com>
2017-07-06 18:46:41 -0700

    Added build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+# generate frontend static assets
+cd frontend
+npm run build
+cd ..
+
+# code-ify static assets
+go get github.com/GeertJohan/go.rice
+go get github.com/GeertJohan/go.rice/rice
+rice embed-go
+
+# compile goldfish binary
+env GOOS=linux GOARCH=amd64 go build -o goldfish-linux-amd64 -v github.com/caiyeon/goldfish
