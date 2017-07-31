060f648f9a5b896aefaf9867a254547a212096dc
Tony Cai <081099@gmail.com>
2017-07-30 17:16:51 -0700

    Factored out user.go

--- vault/approle.go
+++ vault/approle.go
@@ -5,13 +5,6 @@ import (
 	"errors"
 )
 
-type UserpassUser struct {
-	Name     string
-	TTL      int
-	Max_TTL  int
-	Policies string
-}
-
 type Role struct {
 	Roleid             string
 	Token_TTL          int
@@ -22,37 +15,6 @@ type Role struct {
 	Period             int
 	Bind_secret_id     bool
 	Bound_cidr_list    string
-}
-
-func (auth AuthInfo) ListUserpassUsers() ([]UserpassUser, error) {
-	client, err := auth.Client()
-	if err != nil {
-		return nil, err
-	}
-	logical := client.Logical()
-
-	// get a list of usernames
-	resp, err := logical.List("auth/userpass/users")
-	if err != nil {
-		return nil, err
-	}
-	usernames, ok := resp.Data["keys"].([]interface{})
-	if !ok {
-		return nil, errors.New("Failed to convert response")
-	}
-
-	// fetch each user's details
-	users := make([]UserpassUser, len(usernames))
-	for i, username := range usernames {
-		users[i].Name = username.(string)
-		resp, err := logical.Read("auth/userpass/users/" + users[i].Name)
-		if err == nil {
-			if b, err := json.Marshal(resp.Data); err == nil {
-				json.Unmarshal(b, &users[i])
-			}
-		}
-	}
-	return users, nil
 }
 
 func (auth AuthInfo) ListApproleRoles() ([]Role, error) {
