cdc0ec99d57c24e095fb1040672eb5f0f2a87448
Tony Cai <081099@gmail.com>
2017-08-02 22:05:38 -0700

    Updated goldfish policy to reflect optional transit encryption

--- config/dev.go
+++ config/dev.go
@@ -315,21 +315,20 @@ func generateWrappedSecretID(v VaultConfig, token string) (string, error) {
 	return resp.WrapInfo.Token, nil
 }
 
-const goldfishPolicyRules = `
-# [mandatory]
-# credential transit key (stores logon tokens)
-# NO OTHER POLICY should be able to write to this key
+const goldfishPolicyRules = `# [mandatory]
+# store goldfish run-time settings here
+# goldfish hot-reloads from this endpoint every minute
+path "secret/goldfish*" {
+  capabilities = ["read", "update"]
+}
+
+
+# [optional]
+# to enable transit encryption, see wiki for details
 path "transit/encrypt/goldfish" {
   capabilities = ["read", "update"]
 }
 path "transit/decrypt/goldfish" {
-  capabilities = ["read", "update"]
-}
-
-# [mandatory] [changable]
-# store goldfish run-time settings here
-# goldfish hot-reloads from this endpoint every minute
-path "secret/goldfish*" {
   capabilities = ["read", "update"]
 }
 `
--- vagrant/policies/goldfish.hcl
+++ vagrant/policies/goldfish.hcl
@@ -1,16 +1,16 @@
 # [mandatory]
-# server's transit key (stores logon tokens)
-# NO OTHER POLICY should be able to write to this key
+# store goldfish run-time settings here
+# goldfish hot-reloads from this endpoint every minute
+path "secret/goldfish*" {
+  capabilities = ["read", "update"]
+}
+
+
+# [optional]
+# to enable transit encryption, see wiki for details
 path "transit/encrypt/goldfish" {
   capabilities = ["read", "update"]
 }
 path "transit/decrypt/goldfish" {
-  capabilities = ["read", "update"]
-}
-
-# [mandatory] [changable]
-# store goldfish run-time settings here
-# goldfish hot-reloads from this endpoint every minute
-path "secret/goldfish*" {
   capabilities = ["read", "update"]
 }
