615d994ee0369943fc338b25d291b8e35c4a8176
Tony Cai <081099@gmail.com>
2017-08-08 19:32:44 -0700

    Updated unit tests for request system

--- request/request_test.go
+++ request/request_test.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"testing"
 
 	"github.com/caiyeon/goldfish/config"
@@ -23,10 +25,20 @@ func TestRequestSystem(t *testing.T) {
 		panic(err)
 	}
 
-	Convey("Testing request system", t, func() {
-		// this will be imitating the client token
-		rootAuth := &vault.AuthInfo{ID: "goldfish", Type: "token"}
+	// this will be imitating the client token
+	rootAuth := &vault.AuthInfo{ID: "goldfish", Type: "token"}
 
+	// collect requester's information
+	self, err := rootAuth.LookupSelf()
+	if err != nil {
+		panic(err)
+	}
+	if self == nil {
+		panic("Could not confirm requester identity")
+	}
+	rootAuthHash := fmt.Sprintf("%x", sha256.Sum256([]byte(self.Data["display_name"].(string))))
+
+	Convey("Testing request system", t, func() {
 		Convey("Testing policy requests", func() {
 			//-----------------------------------------------------------------
 			// adding a policy req with a new policy name
@@ -41,16 +53,25 @@ func TestRequestSystem(t *testing.T) {
 			// retrieve the request
 			req, err := Get(rootAuth, hash)
 			So(err, ShouldBeNil)
-			polreq, ok := req.(*PolicyRequest)
-			So(ok, ShouldEqual, true)
-			So(polreq, ShouldNotBeEmpty)
+
+			// verify request body
+			So(req, ShouldResemble, &PolicyRequest{
+				Type: "policy",
+				PolicyName: "abc",
+				Previous: "",
+				Proposed: "# this is a sample policy rule",
+				Requester: "token",
+				RequesterHash: rootAuthHash,
+				Required: 3,
+				Progress: 0,
+			})
 
 			// approve the request
-			err = req.Approve(hash, unsealTokens[0])
+			err = Approve(rootAuth, hash, unsealTokens[0])
 			So(err, ShouldBeNil)
-			err = req.Approve(hash, unsealTokens[1])
+			err = Approve(rootAuth, hash, unsealTokens[1])
 			So(err, ShouldBeNil)
-			err = req.Approve(hash, unsealTokens[2])
+			err = Approve(rootAuth, hash, unsealTokens[2])
 			So(err, ShouldBeNil)
 
 			// confirm changes were made
@@ -71,16 +92,13 @@ func TestRequestSystem(t *testing.T) {
 			// retrieve the request
 			req, err = Get(rootAuth, hash)
 			So(err, ShouldBeNil)
-			polreq, ok = req.(*PolicyRequest)
-			So(ok, ShouldEqual, true)
-			So(polreq, ShouldNotBeEmpty)
 
 			// approve the request
-			err = req.Approve(hash, unsealTokens[0])
+			err = Approve(rootAuth, hash, unsealTokens[0])
 			So(err, ShouldBeNil)
-			err = req.Approve(hash, unsealTokens[1])
+			err = Approve(rootAuth, hash, unsealTokens[1])
 			So(err, ShouldBeNil)
-			err = req.Approve(hash, unsealTokens[2])
+			err = Approve(rootAuth, hash, unsealTokens[2])
 			So(err, ShouldBeNil)
 
 			// confirm changes were made
@@ -101,17 +119,48 @@ func TestRequestSystem(t *testing.T) {
 			// retrieve the request
 			req, err = Get(rootAuth, hash)
 			So(err, ShouldBeNil)
-			polreq, ok = req.(*PolicyRequest)
-			So(ok, ShouldEqual, true)
-			So(polreq, ShouldNotBeEmpty)
 
 			// approve the request
-			err = req.Approve(hash, "NotUnseal")
+			err = Approve(rootAuth, hash, "NotUnseal")
 			So(err, ShouldBeNil)
-			err = req.Approve(hash, "NotUnseal")
+			err = Approve(rootAuth, hash, "NotUnseal")
 			So(err, ShouldBeNil)
-			err = req.Approve(hash, "NotUnseal")
+			err = Approve(rootAuth, hash, "NotUnseal")
 			So(err, ShouldNotBeNil)
+
+			// confirm changes were NOT made
+			rules, err = rootAuth.GetPolicy("abc")
+			So(err, ShouldBeNil)
+			So(rules, ShouldEqual, "# this is not the same rule")
+
+			//-----------------------------------------------------------------
+			// rejecting a request halfway
+			hash, err = Add(rootAuth, map[string]interface{}{
+				"Type":       "policy",
+				"policyname": "abc",
+				"rules":      "# this is a new rule",
+			})
+			So(err, ShouldBeNil)
+			So(hash, ShouldNotBeEmpty)
+
+			// retrieve the request
+			_, err = Get(rootAuth, hash)
+			So(err, ShouldBeNil)
+
+			// approve the request
+			err = Approve(rootAuth, hash, "NotUnseal")
+			So(err, ShouldBeNil)
+			err = Approve(rootAuth, hash, "NotUnseal")
+			So(err, ShouldBeNil)
+
+			// reject the request
+			err = Reject(rootAuth, hash)
+			So(err, ShouldBeNil)
+
+			// confirm request no longer exists
+			req, err = Get(rootAuth, hash)
+			So(err, ShouldNotBeNil)
+			So(req, ShouldBeNil)
 
 			// confirm changes were NOT made
 			rules, err = rootAuth.GetPolicy("abc")
