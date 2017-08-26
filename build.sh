75359dbe7653a6d3982d481d63c482f306df2951
Caiyeon <081099@gmail.com>
2017-08-26 14:50:08 -0700

    Update build script to exit on failure

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ cd ..
 # code-ify static assets
 go get github.com/GeertJohan/go.rice || exit 1
 go get github.com/GeertJohan/go.rice/rice || exit 1
-rm -f rice-box.go
+rm -f rice-box.go || exit 1
 rice embed-go || exit 1
 
 # compile goldfish binary
-env GOOS=linux GOARCH=amd64 go build -o goldfish-linux-amd64 -v github.com/caiyeon/goldfish
-env GOOS=windows GOARCH=amd64 go build -o goldfish-windows-amd64.exe -v github.com/caiyeon/goldfish
+env GOOS=linux GOARCH=amd64 go build -o goldfish-linux-amd64 -v github.com/caiyeon/goldfish || exit 1
+env GOOS=windows GOARCH=amd64 go build -o goldfish-windows-amd64.exe -v github.com/caiyeon/goldfish || exit 1
 
 # report build
 echo 'Successfully built ' $(git describe --always --tags)
