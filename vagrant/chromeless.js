e65fcce2c60582d0ff53efe1d31aea9e0d64b8c4
Caiyeon <081099@gmail.com>
2017-12-02 15:52:10 -0800

    Added login navigation and screenshots #184

--- vagrant/chromeless.js
+++ vagrant/chromeless.js
@@ -1,11 +1,27 @@
 const chromeless = new Chromeless({ remote: true })
 
-const screenshot = await chromeless
+// take a screenshot of the home page
+var screenshot = await chromeless
   .setViewport({width: 1920, height: 1080, scale: 1})
   .goto('https://vault-ui.io/#/')
   .wait(500)
   .screenshot()
-
 console.log(screenshot)
+
+// close notification
+await chromeless.click('button[class~="delete"]').wait(500)
+
+// take a screenshot of the login page
+screenshot = await chromeless
+  .click('a[href="#/login"]')
+  .wait('input[type="password"]')
+  .type('goldfish', 'input[type="password"]')
+  .press(13)
+  .wait(500)
+  .screenshot()
+console.log(screenshot)
+
+// close notification
+await chromeless.click('button[class~="delete"]').wait(500)
 
 await chromeless.end()
