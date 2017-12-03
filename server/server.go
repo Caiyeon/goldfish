f75ab25f5372d809a6a515a00de15490072e981c
Tony Cai <081099@gmail.com>
2017-12-03 00:21:14 -0800

    Fixed ciphersuite list #143

--- server/server.go
+++ server/server.go
@@ -164,6 +164,10 @@ func StartListener(listener config.ListenerConfig, assets *rice.Box) {
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 	}
 
 	// if loading certificate from local machine
