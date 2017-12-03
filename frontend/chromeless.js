e34a2f9bdda53f8f9c6b6a00be444a6a7b05d2c0
Caiyeon <081099@gmail.com>
2017-12-02 20:33:30 -0800

    Reject and re-create request in chromeless #184

--- frontend/chromeless.js
+++ frontend/chromeless.js
@@ -41,26 +41,64 @@ async function run() {
     .type('# Propose changes to policies, and ask admins to approve!', 'textarea')
     .click('div p a[class*="button is-primary is-outlined"]')
     .wait('button[class~="delete"]')
+    .wait(500)
     .click('button[class~="delete"]')
     .wait(500)
 
-  // 4. approve policy change request
+  // 4. reject and re-create the request to force a first-approval scenario
+  // bring up the request via hash
   await chromeless
     .click('a[href="#/requests"]')
     .wait('button[class~="delete"]')
-    .click('button[class~="delete"]')
     .wait(500)
     .click('button[class~="delete"]')
     .wait(500)
-    // this hash should now be valid (after the request creation above)
+    .type('dde8892ad346de84', 'input')
+    .press(13)
+
+  // delete the request
+  await chromeless
+    .wait('article div p button[class*="button is-warning"]')
+    .click('article div p button[class*="button is-warning"]')
+    .wait('article div p button[class*="button is-danger"]')
+    .click('article div p button[class*="button is-danger"]')
+    .wait('button[class~="delete"]')
+    .wait(500)
+    .click('button[class~="delete"]')
+    .wait(500)
+
+  // recreate the request
+  await chromeless
+    .click('a[href="#/policies"]')
+    .wait('a[class*="tag is-primary"]')
+    .click('a[class*="tag is-primary"]')
+
+  await chromeless
+    .wait('textarea')
+    .clearInput('textarea')
+    .type('# Propose changes to policies, and ask admins to approve!', 'textarea')
+    .click('div p a[class*="button is-primary is-outlined"]')
+    .wait('button[class~="delete"]')
+    .wait(500)
+    .click('button[class~="delete"]')
+    .wait(500)
+
+  // 5. approve policy change request
+  await chromeless
+    .click('a[href="#/requests"]')
+    .wait('button[class~="delete"]')
+    .wait(500)
+    .click('button[class~="delete"]')
+    .wait(500)
     .type('dde8892ad346de84', 'input')
     .press(13)
     .wait(500)
 
   screenshot = await chromeless
-    .click('div p button[class*="button is-success"]')
+    .wait('article div p button[class*="button is-success"]')
+    .click('article div p button[class*="button is-success"]')
     .wait('input[placeholder="Enter an unseal key"]')
-    .type('not_a_real_uneal_key', 'input[placeholder="Enter an unseal key"]')
+    .type('not_a_real_unseal_key', 'input[placeholder="Enter an unseal key"]')
     .press(13)
     .wait(500)
     .screenshot()
