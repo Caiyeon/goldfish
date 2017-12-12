0b376ddd5ff15e0d2d72cd47afd57b8e31dc4849
Caiyeon <081099@gmail.com>
2017-12-11 17:04:13 -0800

    Remove github buttons from demo

--- frontend/client/components/layout/FooterBar.vue
+++ frontend/client/components/layout/FooterBar.vue
@@ -3,25 +3,6 @@
     <div class="container">
       <div class="content has-text-centered">
 
-        <!-- NONE of these third party scripts will be in the production binaries -->
-        <!-- github buttons -->
-        <p class="social">
-          <a>
-            <github-button
-            href="https://github.com/caiyeon/goldfish"
-            data-show-count="true"
-            data-size="large"
-            data-icon="octicon-star">Stars</github-button>
-          </a>
-          <a>
-            <github-button
-            href="https://github.com/caiyeon/goldfish/fork"
-            data-show-count="true"
-            data-size="large"
-            data-icon="octicon-repo-forked">Forks</github-button>
-          </a>
-        </p>
-
         <!-- general description -->
         <p><span class="icon"><i class="fa fa-code"></i></span> with <span class="icon"><i class="fa fa-heart"></i></span> by <a href="https://github.com/caiyeon">caiyeon</a></p>
         <p>
@@ -30,19 +11,14 @@
           <a :href="'https://github.com/' + repository + '/blob/master/LICENSE'">{{ license }}</a>
         </p>
         <p>Frontend boilerplate made by <a href="https://github.com/fundon">fundon</a></p>
+
       </div>
     </div>
   </footer>
 </template>
 
 <script>
-import GithubButton from 'github-buttons/dist/vue/GithubButton'
-
 export default {
-  components: {
-    GithubButton
-  },
-
   data () {
     return this.$store.state.pkg
   }
@@ -57,10 +33,6 @@ export default {
 
   @include mobile() {
     margin-left: 0;
-  }
-
-  .social a {
-    border-bottom: none !important;
   }
 
   .fa.fa-heart {
